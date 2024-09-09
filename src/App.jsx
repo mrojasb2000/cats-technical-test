8b2eb540194cb19e0d7bf852bf91c3b704cfc0df
Mauricio Rojas <mrojasb2000@gmail.com>
2024-09-09 19:21:31 -0300

    chore: add fetch data on useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://catas.com/cat/says/${firstWords}?size=50&color=red&json=true`
+
 export function App () {
+  const [fact, setFact] = useState('lorem upsum cat fact whatever')
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
+
   return (
-    <h1>App de Gatitos</h1>
+    <main>
+      <h1>App de Gatitos</h1>
+      <p>{fact}</p>
+    </main>
   )
 }
