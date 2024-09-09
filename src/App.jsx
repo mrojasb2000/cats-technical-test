62afe328ac27a58d6939a3780c43d4fcb63615b3
Mauricio Rojas <mrojasb2000@gmail.com>
2024-09-09 19:30:02 -0300

    chore: add async function on useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,28 @@ const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://catas.com/cat/says/${firstWords}?size=50&color=red&json=true`
 
 export function App () {
-  const [fact, setFact] = useState('lorem upsum cat fact whatever')
+  const [fact, setFact] = useState()
+
+  // useEffect(() => {
+  //   fetch(CAT_ENDPOINT_RANDOM_FACT)
+  //     .then(res => res.json())
+  //     .then(data => setFact(data.fact))
+  // }, [])
 
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => setFact(data.fact))
+    async function getRandomFact () {
+      const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+      const json = await res.json()
+      setFact(json.fact)
+    }
+
+    getRandomFact()
   }, [])
 
   return (
     <main>
       <h1>App de Gatitos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
     </main>
   )
 }
